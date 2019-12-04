9645a329bf8a39643c3e6f97f074da8eada16883
darshan-rt <58321227+darshan-rt@users.noreply.github.com>
2019-12-04 11:46:34 +0000

    Update problem_5.java

--- Problem5/problem_5.java
+++ Problem5/problem_5.java
@@ -6,7 +6,7 @@ class problem_5
     int[] temp = new int[arr.length]; 
     int c = 0;
     
-    for(int i = 0; i < arr.length;i++)
+   for(int i = 0; i < arr.length;i++)
     {
       if(arr[i] != 0)
 	  {
